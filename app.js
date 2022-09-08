d540ec8e5e7928f13c2022d910058e895055b0dc
unknown <daveasmah@yahoo.com>
2022-09-07 22:29:54 -0400

    Final commit

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 //Colors-Swticher
-
-//Declaring a variable for ID mySelect
-const colors  = document.getElementById('mySelect');
-
-//callback function
-changeColor(colors);
-
 //function to change background color once option is changed
 function changeColor() {
+
+    //Declaring a variable for ID mySelect
+    const colors  = document.getElementById('mySelect');
     let x = colors.value;
     document.body.style.background = x;
 }    
